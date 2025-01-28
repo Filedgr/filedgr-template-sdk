0ca6927fb18c0764adefb96d52cc2c2af121ac98
moji <m.parvar@filedgr.com>
2025-01-28 22:46:55 +0330

    chore: fix import issue

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
+// src/index.ts
 import { FiledGrTemplateSDK as FiledGrTemplateSDKClass } from './async'
 import { BaseAsync } from './base'
 import { applyMixins } from './utils'
+
+// Re-export everything from constants and types in a namespace
+import * as Constants from './constants'
+import * as Types from './types'
 
 class FiledGrTemplateSDK extends BaseAsync {}
 interface FiledGrTemplateSDK extends FiledGrTemplateSDKClass {}
 
 applyMixins(FiledGrTemplateSDK, [FiledGrTemplateSDKClass])
 
+// Export the namespace
+export { Constants, Types }
+
+// Keep the default export
 export default FiledGrTemplateSDK
